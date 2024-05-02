9983850a40a6adefe7467df84575707c9bbc9d8d
ValmirNogueira <valmirnogueiraifrn@hotmail.com>
2024-05-02 09:02:22 -0300

    Iniciando projeto de conta bancária

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO; Conhecer e importar a classe Scanner
+
+        // Exibr as mensagens para o nosso usuário
+
+        // Obter oela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada .
     }
 }
