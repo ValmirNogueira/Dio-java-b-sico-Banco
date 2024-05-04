7396e052ca5b69b8ed409473b5132a5b447d1a77
ValmirNogueira <valmirnogueiraifrn@hotmail.com>
2024-05-04 20:52:38 -0300

    Meu Banco

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // TODO; Conhecer e importar a classe Scanner
@@ -7,5 +9,32 @@ public class ContaTerminal {
         // Obter oela scanner os valores digitados no terminal
 
         // Exibir a mensagem conta criada .
+        Scanner scanner = new Scanner(System.in);
+        String nome; 
+        String sobreNome;
+        int numeroConta;
+        int valorDeposito;
+        int agencia = 1234;
+
+
+        System.out.println("Olá, seja muito bem vindo ao Valbank"); 
+        
+        System.out.println("Para darmos incíco a abertura de sua conta, por gentileza digite o seu primeiro nome:");
+        nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome :");
+        sobreNome = scanner.next();
+
+        System.out.println("Agora escola 4 digitos não sequênciais para serem o numero da sua nova conta :");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("Escola uma valor incial para depósito:");
+        valorDeposito = scanner.nextInt();
+
+        System.out.println("Olá " + nome + " " + sobreNome + ", obrigado por criar uma conta em nosso banco, sua agência é :" + agencia);
+        System.out.println("Conta :" + numeroConta + " e seu saldo :" + valorDeposito + " já está disponível para saque");
+
+
+
     }
 }
